b51cbccd43c201ba82a372cd72abd20c39b04ec8
Marcelo Ranzani <raniten@gmail.com>
2023-11-12 18:09:52 -0300

    Paquetes creados y clases Users vacías
    Se crearon las clases User vacías para diagramar el proyecto

---
+++ C23650G1/src/main/java/com/cac/C23650G1/C23650G1Application.java
@@ -0,0 +1,13 @@
+package com.cac.C23650G1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class C23650G1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(C23650G1Application.class, args);
+	}
+
+}
---
+++ C23650G1/src/test/java/com/cac/C23650G1/C23650G1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cac.C23650G1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class C23650G1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
