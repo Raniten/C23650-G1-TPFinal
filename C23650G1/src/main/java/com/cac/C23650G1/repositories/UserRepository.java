eae0cebdcf8a69f5a60a6f27c3cb8de2e2ffe645
Javier Arans <javier.arans@gmail.com>
2023-11-13 16:13:27 -0300

    Actualice el driver de Mysql
    Actualice el driver mysql y comente algunas líneas para comprobar que el proyecto funcione, por que no podía lanzar la app.

--- C23650G1/src/main/java/com/cac/C23650G1/repositories/UserRepository.java
+++ C23650G1/src/main/java/com/cac/C23650G1/repositories/UserRepository.java
@@ -4,6 +4,6 @@ import com.cac.C23650G1.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface UserRepository extends JpaRepository <User, Long> {
+//@Repository
+public interface UserRepository  {
 }
