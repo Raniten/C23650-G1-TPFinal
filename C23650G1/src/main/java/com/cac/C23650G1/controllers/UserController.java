ec581fbb135b622b92f5671066b386bc74b30ea4
Javier Arans <javier.arans@gmail.com>
2023-11-21 15:48:42 -0300

    Primer Commit
    Consulta de users

--- C23650G1/src/main/java/com/cac/C23650G1/controllers/UserController.java
+++ C23650G1/src/main/java/com/cac/C23650G1/controllers/UserController.java
@@ -1,4 +1,33 @@
 package com.cac.C23650G1.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+//Establecemos que mapee las request por defecto tenemos (http://localhost:8080)
+// en la siguiente linea le indicamos que todas las request deben contener /api/users
+@RequestMapping(value = "/api/users")
+// todos los metodos van a ser /api/user, solo cambia al consultar userById, editar o borrar un user
 public class UserController {
+
+     // Necesito obtener una instancia del servicio de usuarios
+
+    @Autowired
+    private UserService service;
+
+
+    // Vamos a obtener una lista de usuarios registrados
+
+    @GetMapping //Para acceder a este recurso deben colocar "/users"
+    public ResponseEntity<List<String>> getusers(){
+        return ResponseEntity.status(HttpStatus.OK).body(service.getusers());
+    }
+
+
 }
