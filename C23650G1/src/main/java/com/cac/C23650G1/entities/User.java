f744d7330d9299f9816fa9a6dcb7c8a253c780a3
Javier Arans <javier.arans@gmail.com>
2023-12-07 19:46:04 -0300

    Update User
    Genere: User Entities, UserDto, UserMapper, UserService, User Repository y las valiaciones para la creacion de un usuario

---
+++ C23650G1/src/main/java/com/cac/C23650G1/entities/User.java
@@ -0,0 +1,54 @@
+package com.cac.C23650G1.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Entity
+@Table(name = "homebanking_user")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String username;
+
+    private String password;
+
+    private String firstname;
+
+    private String lastname;
+
+    @Column(unique = true)
+    private String dni;
+
+    private String email;
+
+    private String address;
+
+    @Column(name = "fecha_nacimiento")
+    private Date birthday_date;
+
+    @Column(name = "fecha_creacion")
+    private LocalDateTime created_at;
+
+    @Column(name = "fecha_modificacion")
+    private LocalDateTime updated_at;
+
+   // @OneToMany(mappedBy = "owner", cascade = CascadeType.ALL, orphanRemoval = true)
+    //private List<Account> accounts;
+
+
+}
+
+
