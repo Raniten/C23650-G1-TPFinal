a2f9f97ce904085589f38286d27a7a47605a89c0
agustin vargas <agusvargas57@gmail.com>
2023-12-14 01:10:37 -0300

    Creacion de transfer y account repository

--- C23650G1/src/main/java/com/cac/C23650G1/repository/AccountRepository.java
+++ C23650G1/src/main/java/com/cac/C23650G1/repository/AccountRepository.java
@@ -1,4 +1,10 @@
 package com.cac.C23650G1.repository;
 
-public interface AccountRepository {
+import com.cac.C23650G1.entities.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface AccountRepository extends JpaRepository<Account,Long> {
 }
--- C23650G1/src/main/java/com/cac/C23650G1/repository/TransferRepository.java
+++ C23650G1/src/main/java/com/cac/C23650G1/repository/TransferRepository.java
@@ -1,4 +1,11 @@
 package com.cac.C23650G1.repository;
 
-public interface TransferRepository {
+import com.cac.C23650G1.entities.Transfer;
+import com.cac.C23650G1.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface TransferRepository extends JpaRepository<Transfer, Long> {
 }
